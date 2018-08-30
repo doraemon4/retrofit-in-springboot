9bb7a20018994f2693f1a09511ebabaa553a71bc
jack <zhangkun@51dojo.com>
2018-08-30 23:25:54 +0800

    关闭响应

--- retrofit-consumer/src/main/java/com/stephen/learning/retrofit/util/HttpclientUtil.java
+++ retrofit-consumer/src/main/java/com/stephen/learning/retrofit/util/HttpclientUtil.java
@@ -2,7 +2,7 @@ package com.stephen.learning.retrofit.util;
 
 import com.alibaba.fastjson.JSONArray;
 import com.stephen.learning.retrofit.config.HttpConnectionManager;
-import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
@@ -24,14 +24,23 @@ public class HttpclientUtil {
     public <T> List<T> get(String url, Class<T> clazz){
         CloseableHttpClient httpClient=connManager.getHttpClient();
         HttpGet request = new HttpGet(url);
+        CloseableHttpResponse httpResponse=null;
         try{
-            HttpResponse httpResponse = httpClient.execute(request);
+            httpResponse = httpClient.execute(request);
             if (httpResponse.getStatusLine().getStatusCode() == 200) {
                  String result=EntityUtils.toString(httpResponse.getEntity(), "utf-8");
                  return JSONArray.parseArray(result,clazz);
             }
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            if(httpResponse!=null){
+                try{
+                    httpResponse.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
         }
         return null;
     }
